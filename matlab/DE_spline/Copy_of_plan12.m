05b3d9bff2bafb602ac7d70735fba6f3192b2043
kimrla <532682970@qq.com>
2020-09-16 20:52:40 +0800

    1

--- matlab/DE_spline/Copy_of_plan12.m
+++ matlab/DE_spline/Copy_of_plan12.m
@@ -1,16 +1,24 @@
 clear all;
+
+NP=50;%种群规模NP
+
+GM=200;%最大迭代次数
+
 % 实验1
 x=0:0.01:1-0.01;
 f=90./(1+exp(-100*(x-0.4)));
-n=8;%控制顶点个数-1
+n=8;
+lamda=0.025;%节点率λ
+dp=0.6;%删除概率
 N=length(x);%采样点个数
 
 % % 实验2
 % x=0:0.05:10;
-% f=100./exp(abs(x-5))+(x-5).^2/500;
+% y=100./exp(abs(x-5))+(x-5).^2/500;
 % n=13;
 
-f_=f+normrnd(0,0.01,1,N);
+f_=f+normrnd(0,0.01,1,N);%加随机扰动
+
 
 plot(x,f_,"*")
 hold on
@@ -22,10 +30,11 @@ M=length(x)-1; %论文中是0~M，所以总数length=M+1
 t=canshuhua(M,d);
 p=3;%B样条次数p=3，控制顶点n+1个，节点矢量ui i=1~n+p+2,
 
-
-
-
-ui=jiedianxiangliang(n,p);%方案1 均匀节点向量
+%初始种群
+ui=zeros(NP,n+p+2);
+for i=1:NP
+    ui(i,:)=sort(jiedianxiangliang(n,p)+[zeros(1,4) normrnd(0,0.01,1,n-3) zeros(1,4)]);%方案1 均匀节点向量
+end
 
 
 [N,R,P] = kongzhidingdian(M,n,p,t,ui,d);
--- matlab/DE_spline/plan34.m
+++ matlab/DE_spline/plan34.m
@@ -1,6 +1,6 @@
 clear all;
 % 实验3
-[x,y]=meshgrid(0:0.01:1);
+[x,y]=meshgrid(0:0.3125:1);
 f=74*(x>2/3&x<=1&y>=0&y<=1/3)+76*((x>=1/3&x<=2/3&y>=0&y<=1/3)|(x>2/3&x<=1&y>1/3&y<=2/3))+...
     78*((x>=0&x<=1/3&y>=0&y<=1/3)|(x>1/3&x<=2/3&y>1/3&y<=2/3)|(x>2/3&x<=1&y>2/3&y<=1))+...
     80*((x>=0&x<=1/3&y>1/3&y<=2/3)|(x>1/3&x<=2/3&y>2/3&y<=1))+...
@@ -9,29 +9,31 @@ nu=15;
 nv=15;
 
 % % 实验4
-% [x,y]=meshgrid(0:0.01:1);
+% [x,y]=meshgrid(0:0.1875:1);
 % f=exp(-0.04*((80.*x-40).^2+(90.*y-45).^2).^0.5).*cos(0.15*((80.*x-40).^2+(90.*y-45).^2).^0.5);
 % nu=9;
 % nv=9;
+
 plot3(x,y,f,"*")
 mesh(x,y,f)
 hold on
-d=[x;y]';%给定数据点,di=(xi,yi) i=1~M+1
+dx=[x;f]';%给定数据点,di=(xi,yi) i=1~M+1
+dy=[y;f]';
 
-
-M=length(x)-1; %论文中是0~M，所以总数length=M+1
-
-t=canshuhua(M,d);
+Mx=length(x)-1; %论文中是0~M，所以总数length=M+1
+My=length(y)-1;
+tx=canshuhua(Mx,dx);
+ty=canshuhua(My,dy);
 p=3;%B样条次数p=3，控制顶点n+1个，节点矢量ui i=1~n+p+2,
 
 
 
 
-ui=jiedianxiangliang(n,p);%方案1 均匀节点向量
+ui=jiedianxiangliang(nu,p);% 均匀节点向量
+vi=jiedianxiangliang(nv,p);
 
-
-[N,R,P] = kongzhidingdian(M,n,p,t,ui,d);
-
-DrawSpline(n,p,P,ui);
-
-[epsilon,e] = shujudianwucha(M,N,P,d);
+[Nu,~,Pu] = kongzhidingdian(Mx,nu,p,tx,ui,dx);
+[Nv,~,Pv] = kongzhidingdian(My,nv,p,ty,vi,dy);
+DrawSpline(nu,p,Pu,ui);
+DrawSpline(nv,p,Pv,vi);
+% [epsilon,e] = shujudianwucha(M,N,P,d);
