73d2ddff7dc9c2e41ea397448fe4efa34293aab0
kimrla <532682970@qq.com>
2020-09-22 16:07:21 +0800

    1

--- matlab/DE_spline/plan12.m
+++ matlab/DE_spline/plan12.m
@@ -9,12 +9,18 @@ N=length(x);%采样点个数
 % x=0:0.05:10;
 % f=100./exp(abs(x-5))+(x-5).^2/500;
 % n=13;
+% N=length(x);%采样点个数
 
 f_=f+normrnd(0,0.01,1,N);
 
+
+
 plot(x,f_,"*")
 hold on
+% plot(x,f)
 d=[x;f_]';%给定数据点,di=(xi,yi) i=1~M+1
+
+
 
 
 M=length(x)-1; %论文中是0~M，所以总数length=M+1
@@ -26,6 +32,15 @@ p=3;%B样条次数p=3，控制顶点n+1个，节点矢量ui i=1~n+p+2,
 
 
 ui=jiedianxiangliang(n,p);%方案1 均匀节点向量
+
+
+% u=0:0.01:1;
+% for j=1:n+1
+%     for i=1:length(u)
+%         Njp_u(i,j) = Njp(j, p , u(i), ui);  
+%     end
+%     subplot(5,5,j),plot(u,Njp_u(:,j));
+% end
 
 
 [N,R,P] = kongzhidingdian(M,n,p,t,ui,d);
