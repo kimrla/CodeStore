22702a52fee0fedf63fe1770bac0668219daef66
kimrla <532682970@qq.com>
2020-10-21 12:21:42 +0800

    1

--- matlab/UVsystem/uvbfshow.m
+++ matlab/UVsystem/uvbfshow.m
@@ -9,12 +9,24 @@ x=0:0.001:1;
 %     end
 % end
 %     
-n=3;
 
-
-for k=1:2^(n-1)
-    for i=1:length(x)
-        unk_x(i)=unk(n,k,x(i));
+nmax=3;
+num=1;
+for n=0:nmax
+%     figure
+    if n==0
+     for i=1:length(x)
+            unk_x(i)=unk(n,0,x(i));
     end
-    subplot(5,5,k),plot(x,unk_x)
+    subplot(2^nmax,1,1),plot(x,unk_x),ylabel(["n=",n]);
+    else
+    for k=1:2^(n-1)
+        for i=1:length(x)
+            unk_x(i)=unk(n,k,x(i));
+        end
+        num=num+1;
+        subplot(2^nmax,1,num),plot(x,unk_x),ylabel(["n=",n,"k=",k]);
+    end
+    end
 end
+
