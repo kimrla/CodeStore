edcbc9b4a746c90fe47cb60613c360d9a8c25c5b
kimrla <532682970@qq.com>
2021-03-16 21:43:05 +0800

    1

--- matlab/vfitcurve/fitting.m
+++ matlab/vfitcurve/fitting.m
@@ -1,12 +1,12 @@
 clear
-example=3;
+example=1;
 switch example
     case 1
         % 实验1
         x=0:0.005:1-0.005;
-        f=90./(1+exp(-100*(x-0.4)));        
+        f=90./(1+exp(-100*(x-0.5)));        
         M=length(x);%采样点个数
-        N=128;%基函数个数
+        N=32;%基函数个数
         xmax=1;
     case 2
         % 实验2
@@ -18,18 +18,19 @@ switch example
     case 3
         %
         x=0:0.005:1-0.005;
-        f=1./(0.01+(x-0.3).^2).*(x<0.6)+1./(0.015+(x-0.65).^2).*(x>=0.6);
+        f=1./(0.01+(x-0.3).^2).*(x<0.5)+1./(0.015+(x-0.65).^2).*(x>=0.5);
         M=length(x);
-        N=16;%基函数个数
+        N=32;%基函数个数
         xmax=1;
 end
 f_=f+normrnd(0,1,1,M);
 
-k=3;%次数
+k=1;%次数
 % t=canshuhua(M,d);
 t=x/xmax;
 d=[t;f_]';
 [V,lamda]=vxishu(k,N,M,t,d);
+[V1,lamda1]=vdxishu(k,N,M,t,d);
 plot(t,f,'*')
 hold on
 drawV(N, k, lamda);
---
+++ matlab/vfitcurve/vdxishu.m
@@ -0,0 +1,27 @@
+function [V,lamda] = vdxishu(k,N,M,t,d)
+%VXISHU 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+for n=1:N
+    %第N个V基函数是第n组的第i类的第j个V基 其中n=log2(N/(k+1))+1向上取整, i=N-(k+1)*2^(n-2)除2^(n-2)的商向上取整，取余为j
+        if n<=k+1
+            group=1;
+            i=n;
+            j=1;
+        else            
+            group=ceil(log2(n/(k+1))+1);
+            i=ceil((n-(k+1)*2^(group-2))/2^(group-2));
+            j=mod((n-(k+1)*2^(group-2)),2^(group-2));
+            if j==0
+                    j=2^(group-2);
+            end
+        end
+    for m=1:M        
+        V(m,n)=vknij(k,group,i,j,t(m));         
+    end
+    
+    D(n,p)
+end
+lamda=pinv(transpose(V)*V)*transpose(V)*d;
+end
+
