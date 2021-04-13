8cc35e5fa5776ed19a9418d3f565eafa321fb5b5
kimrla <532682970@qq.com>
2021-04-13 19:57:15 +0800

    1

--- matlab/vfitcurve/fitting.m
+++ matlab/vfitcurve/fitting.m
@@ -23,7 +23,7 @@ switch example
 %         N=32;%基函数个数
         xmax=1;
     case 4        
-        x=0:0.05:10-0.05;        
+        x=0:0.02:10-0.02;        
         y=@(x) 100./exp(abs(x-5))+(x-5).^5/500;
         wide=1;
         for i=1:length(x)
@@ -50,10 +50,10 @@ f=f';
 % %     subplot(N,1,i),plot(x,V(:,i))
 % % end
 k = 1;
-N = 5;
+N = 7;
 A = LSMatrix_V(k,N,t);
 
-plan=1;
+plan=3;
 switch plan
     case 1
 % % Show
@@ -114,7 +114,92 @@ Y = M\by;
 
 X = X(1:VNum);
 Y = Y(1:VNum);
+    case 3
+        X = A\t;
+        Y=A\f;
+        % figure
+        % bar(abs(Y))
+        % 找到最大的lamda
+        lastN=abs(Y((k+1)*2^(N-2)+1:(k+1)*2^(N-1)));
+        tezhengV=find(lastN>=mean(lastN));
+        tezhengV=sort(tezhengV);
+        for i=1:length(tezhengV)
+             tezhengV(i)=mod(tezhengV(i),2^(N-2));
+                if  tezhengV(i)==0
+                    tezhengV(i)=2^(N-2);
+                end
+        end
+        % tezhengV  = [1 2 9 10 11 12 13 17 18 20 21 23 24];
+        arset{1}=tezhengV(1);
+        a=1;
+        for i=2:length(tezhengV)
+            if tezhengV(i)-tezhengV(i-1)==1
+                arset{a}(end+1)=tezhengV(i);
+            else
+                a=a+1;
+                arset{a}=tezhengV(i);
+            end
+        end
+        for i=1:a
+            m=1;
+            for j=1:length(arset{i})
+%                 arset{i}(j)=mod(arset{i}(j),2^(N-2));
+%                 if  arset{i}(j)==0
+%                     arset{i}(j)=2^(N-2);
+%                 end
+
+            position{i}(m)=(1+(arset{i}(j)-1)*2)/(2^(N-1));
+            m=m+1;
+            end
+            tz(i)=mean(position{i});
+        end
+        % [group,i,j] = findv(n,k);
+        % position=(2*j-1)/2^(group-1);
+        [DR,DL] = VContinuityInfo1(N);
+
+        % CList = [1/8,1/8,0;
+        %             0.25,0.25,0;
+        %             3/8,3/8,0;
+        %             0.5,0.5,0;
+        %             5/8,5/8,0;
+        %             0.75,0.75,0;
+        %             7/8,7/8,0;];
+             CList=zeros(2^(N-1)-1,3);
+             CList(:,1)=1/(2^(N-1)):1/(2^(N-1)):(2^(N-1)-1)/(2^(N-1));
+             CList(:,2)=CList(:,1);
+        %       CList(16,:)=[];
+             CList(ismember(CList(:,1),tz),:)=[];
+             p = sum(CList(:,3)+1);  % 约束方程数目
+        SegNum = 2^(N-1); % 分段数
+        VNum = 2^N;       % 基函数数目
+        knots = linspace(0,1,SegNum+1);% 节点向量
+        C = zeros(p,VNum);    % 约束矩阵
+        csidx = 0; 
+        for h = 1 : length(CList)           % 给每个约束建立相应的方程组
+            x1 = find(knots==CList(h,1));   % 左节点
+            x2 = find(knots==CList(h,2));   % 右节点
+            for c = 0 : CList(h,3)          % 从C^0约束至C^CList(h,3)约束，每个约束建立一个方程
+                csidx = csidx + 1;
+                C(csidx,:) = [DR(:,x1,c+1) - DL(:,x2,c+1)]';
+            end
+        end  
+
+        M = [2*A'*A,C';
+             C,zeros(p)];
+         d = zeros(p,1);
+        bx = [2*A'*t;d]; 
+        by = [2*A'*f;d]; 
+
+        X = M\bx;
+        Y = M\by;
+
+        X = X(1:VNum);
+        Y = Y(1:VNum);
+
 end
+
+
+
 
 InfoV1Bas = BaseGene_V1(N);   % 1次V系统基函数信息(非离散采样)
 [VRInfo,NumSeg] = VReconstruction_Polyline([X Y],InfoV1Bas);
@@ -124,10 +209,3 @@ for j = 1 : NumSeg
     plot(VRInfo(j,5:6),VRInfo(j,7:8),'Color',[0 102 153]/255,'LineWidth',1.5);hold on
 end
 xlim([0 1])
-figure
-bar(abs(Y))
-%找到最大的lamda
-lastN=abs(Y((k+1)*2^(N-2)+1:(k+1)*2^(N-1)));
-
-% [group,i,j] = findv(n,k);
-% position=(2*j-1)/2^(group-1);
